baaa53bee33c84cc0ee88e1c220c781349d23c40
E. Spangler <ernest.spangler@gmail.com>
2018-11-15 12:20:23 -0500

    v1.00 - release 3
    Industry codes applications - v1.00 - release 3

---
+++ industry-codes-framework/IndustryCodes.Clean.ps1
@@ -0,0 +1,19 @@
+cd  IndustryCodes
+del bin
+del obj
+cd ..
+
+cd  IndustryCodes.Database
+del bin
+del obj
+cd ..
+
+cd  IndustryCodes.Database.Entities
+del bin
+del obj
+cd ..
+
+cd  IndustryCodes.Utility
+del bin
+del obj
+cd ..
